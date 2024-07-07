c5cfc037680d8a4f3fff206b3b737dc6218dad29
angel luna <angelstivengarcialuna@gmail.com>
2024-07-06 22:59:01 -0500

    subida de imagenes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,39 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
+const multer = require("multer");
+const sharp = require("sharp");
 
 // Middleware para parsear el body de las solicitudes
-app.use(express.json()); 
+app.use(express.json());
+app.use(cors());
 
+// Configuración de almacenamiento de multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public"); // TODO: imagen cruda
+    },
+    filename: (req, file, cb) => {
+        const ext = file.originalname.split(".").pop(); // TODO: imagen.png --> .png
+        cb(null, `${Date.now()}.${ext}`);
+    }
+});
+
+const upload = multer({ storage });
+
+app.post("/public", upload.single('file'), (req, res) => {
+    res.send({ data: "Imagen cargada" });
+});
 
 // Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Conectado a MongoDB Atlas"))
   .catch((e) => console.error(`Error al conectar a MongoDB: ${e}`));
 
 // Rutas
 const productoRoutes = require("./routes/productos.routes");
-const categoriaRoutes = require("./routes/caterias.routes");
+const categoriaRoutes = require("./routes/categorias.routes");
 
 app.use("/api", productoRoutes);
 app.use("/api", categoriaRoutes);
@@ -23,5 +43,10 @@ app.get("/", (req, res) => {
   res.send("¡Bienvenido a la API!");
 });
 
-module.exports = app;
+// Iniciar el servidor en el puerto 3000
+const PORT = process.env.PORT ;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
+module.exports = app;
