960ddf5111aa464ecae5d54a55870a469318ed42
angel luna <angelstivengarcialuna@gmail.com>
2024-08-03 23:20:36 -0500

    Listo Arreglar controladores y rutas de productos

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,17 +1,13 @@
 const { Router } = require('express');
-const { actualizarCategoria, crearCategoria, eliminarCategoria, obtenerCategoriaPorId, traerTodasLasCategorias } = require('../controllers/controlador.controlador');
+const { actualizarCategoria, crearCategoria, eliminarCategoria, obtenerCategoriaPorId, traerTodasLasCategorias } = require('../controllers/categoria.controlador');
 const enrutador = Router();
-
 // GET
 enrutador.get('/categorias', traerTodasLasCategorias);
 enrutador.get('/categoria/:id', obtenerCategoriaPorId);
-
 // POST
 enrutador.post('/categoria', crearCategoria);
-
 // PUT
 enrutador.put('/categoria/:id', actualizarCategoria);
-
 // DELETE
 enrutador.delete('/categoria/:id', eliminarCategoria);
 
